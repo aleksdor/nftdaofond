9d6a2c293c34ee87a214f9a8f9d54275481a8ed9
SeregaBiceps <58617773+SeregaBiceps@users.noreply.github.com>
2021-12-09 19:20:07 +0300

    Update Header.jsx

--- front/src/components/Page/Header.jsx
+++ front/src/components/Page/Header.jsx
@@ -1,8 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { NavLink } from 'react-router-dom'
 import imgheader04 from '../../assets/img/icon-header/imgheader04.ico';
+import api from '../../api/api'
 
 const Header = () => {
+	const [connected, setConnected] = useState(api.connected)
+	const [address, setAddress] = useState(api.accountAddress)
+	const [balance, setBalance] = useState(0)
+
+	const connect = () => {
+		api.connect(({ address, success }) => {
+			setAddress(address)
+			setConnected(success)
+		})
+	}
+
+	useEffect(() => {
+		api.connectRpc(async () => {
+			const { history: { bids } } = await api.getHistory()
+			const reducer = (previousValue, currentValue) => Number(previousValue) + Number(currentValue)
+			const balance = bids.reduce(reducer) / 1000000000000000000
+			setBalance(balance)
+		})
+	}, [])
+
 	return (
 		<nav class="navbar navbar-expand-lg navbar-light navbar-bg">
 			<div className="container-lg">
@@ -11,11 +32,11 @@ const Header = () => {
 				</a>
 				<div className="justify-content-end navbar-collapse collapse">
 					<div className="routes">
-						<NavLink to={'/'} className="route ">TREASURY Ξ 14413</NavLink>
+						<span className="route ">TREASURY Ξ {balance}</span>
 						<NavLink to={'/'} className="route ">MAIN</NavLink>
 						<NavLink to={'/vote'} className="route ">VOTE</NavLink>
 						<NavLink to={'/playground'} className="route ">PLAYGROUND</NavLink>
-						<NavLink to={'/'} className="route ">CONNECT WALLET</NavLink>
+						<span className="route " onClick={connect}>{connected ? api.spliceAddress(address) : 'CONNECT WALLET'}</span>
 					</div>
 				</div>
 			</div>
