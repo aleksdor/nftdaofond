cd518e21bd6d671149142b776e558baeac942e24
SeregaBiceps <58617773+SeregaBiceps@users.noreply.github.com>
2021-12-09 19:20:44 +0300

    Update NounInfo.jsx

--- front/src/components/Main/NounInfo.jsx
+++ front/src/components/Main/NounInfo.jsx
@@ -2,16 +2,71 @@ import React, { useState } from 'react';
 import { Modal } from 'react-bootstrap';
 import main01 from '../../assets/img/main01.png';
 import MiniCard from '../MiniCard';
-
-
-
-
+import api from '../../api/api'
 
 const NounInfo = () => {
 	const [show, setShow] = useState(false);
-
+	const [bid, setBid] = useState(0);
+	const [currentTokenImage, setCurrentTokenImage] = useState('')
+	const [currentId, setCurrentId] = useState(0)
+	const [bids, setBids] = useState([])
+	const [endsAt, setEndsAt] = useState('')
+	const [bidders, setBidders] = useState([])
+	const [hours, setHours] = useState('')
+	const [minutes, setMinutes] = useState('')
+	const [seconds, setSeconds] = useState('')
+	const [minimalBid, setMinimalBid] = useState(0)
 	const handieClose = () => setShow(false);
 	const handleShow = () => setShow(true);
+
+	const startTimer = (endTimestamp) => {
+		const calc = setInterval(timer, 1000)
+		function timer() {
+			const endAt = new Date(endTimestamp);
+			const currentTime = new Date();
+
+			const totalSeconds = Math.floor((endAt - (currentTime)) / 1000)
+			const totalMinutes = Math.floor(totalSeconds / 60)
+			const totalHours = Math.floor(totalMinutes / 60)
+			const totalDays = Math.floor(totalHours / 24)
+
+			const hours = totalHours - (totalDays * 24);
+			const minutes = totalMinutes - (totalDays * 24 * 60) - (hours * 60);
+			const seconds = totalSeconds - (totalDays * 24 * 60 * 60) - (hours * 60 * 60) - (minutes * 60);
+
+			setHours(hours.toString())
+			setMinutes(minutes.toString())
+			setSeconds(seconds.toString())
+		}
+	}
+
+	useEffect(() => {
+		api.connectRpc(async () => {
+			const { tokenImage, currentId } = await api.getCurrentTokenInfo()
+			const { history: { bids, end_at, bidders } } = await api.getHistory()
+			const numberBidsArr = bids.map((bid) => Number(bid) / 1000000000000000000)
+			const minimalBidded = Math.max.apply(Math, numberBidsArr)
+			const minimalBid = minimalBidded / 100 * 50 + minimalBidded
+			setMinimalBid(minimalBid)
+			startTimer(Number(end_at))
+			setEndsAt(end_at)
+			setBids(bids)
+			setBidders(bidders)
+			setCurrentId(currentId)
+			setCurrentTokenImage(tokenImage)
+		})
+	}, [])
+
+	const createBid = async (bid) => {
+		await api.createBid(bid)
+		const { history: { bids, bidders } } = await api.getHistory()
+		const numberBidsArr = bids.map((bid) => Number(bid) / 1000000000000000000)
+		const minimalBidded = Math.max.apply(Math, numberBidsArr)
+		const minimalBid = minimalBidded / 100 * 50 + minimalBidded
+		setMinimalBid(minimalBid)
+		setBids(bids)
+		setBidders(bidders)
+	}
 
 	return (
 		<>
@@ -27,7 +82,7 @@ const NounInfo = () => {
 					<Modal.Title h4 className="auction">
 						<h1 className="modal-descr">
 							Flour
-							113
+							{currentId}
 							<br />
 							Bid History
 						</h1>
@@ -35,7 +90,7 @@ const NounInfo = () => {
 					<button type="button" class="btn-close close" aria-label="Close" onClick={handieClose}></button>
 				</Modal.Header>
 				<Modal.Body>
-					<MiniCard />
+					<MiniCard bidders={bidders} bids={bids} />
 				</Modal.Body>
 			</Modal>
 			<div className="bg">
@@ -44,7 +99,7 @@ const NounInfo = () => {
 						<div className="auction-content col-lg-6">
 							<div className="auction-wrapper">
 								<div className="auction-img">
-									<img src={main01} alt="" className="img-noun" />
+									<img src={currentTokenImage} alt="" className="img-noun" />
 								</div>
 							</div>
 						</div>
@@ -53,16 +108,16 @@ const NounInfo = () => {
 								<div className="auction-information">
 									<div className="auction-activity row">
 										<div className="col-lg-12">
-											<h4 id="spanDate" className="data">Dec 06 2021</h4>
+											<h4 id="spanDate" className="data">{new Date(Date.now()).toDateString().slice(4)}</h4>
 										</div>
 										<div className="auction-title col-lg-12">
 											<div className="auction-descr">
-												<span className="name">Flour 113</span>
+												<span className="name">Flour {currentId}</span>
 											</div>
-											<div className="auction-navigation">
+											{/* <div className="auction-navigation">
 												<button type="button" class="auction-navigation-left">←</button>
 												<button type="button" class="auction-navigation-right">→</button>
-											</div>
+											</div> */}
 										</div>
 									</div>
 									<div className="auction-activity row">
@@ -71,7 +126,7 @@ const NounInfo = () => {
 												<h4 className="carrent-title">Current bid</h4>
 												<h2 className="carrent-descr">
 													Ξ
-													75.00
+													{Number(bids[bids.length - 1]) / 1000000000000000000}
 												</h2>
 											</div>
 										</div>
@@ -80,19 +135,19 @@ const NounInfo = () => {
 											<h2 className="timer-descr">
 												<div id="timer" className="section-timer">
 													<span id="hours">
-														14
+														{hours}
 														<span className="hours">h</span>
 													</span>
 												</div>
 												<div className="section-timer">
 													<span id="minutes">
-														25
+														{minutes}
 														<span className="minutes">m</span>
 													</span>
 												</div>
 												<div className="section-timer">
 													<span id="seconds">
-														8
+														{seconds}
 														<span className="seconds">s</span>
 													</span>
 												</div>
@@ -101,139 +156,59 @@ const NounInfo = () => {
 									</div>
 									<div className="auction-activity row">
 										<div className="col-lg-12">
-											<span className="mini-bid">Minimum bid: 61.2 ETH</span>
+											<span className="mini-bid">Minimum bid: {minimalBid} ETH</span>
 											<div className="input-group">
-												<input class=" group form-control" type="text" aria-label="default input example"></input>
+												<input onChange={(e) => setBid(Number(e.target.value) * 1000000000000000000)} class=" group form-control" type="text" aria-label="default input example"></input>
 												<span className="group-title">ETH</span>
-												<button type="button" class="button-main btn btn-primary">Bid</button>
+												<button onClick={() => createBid(bid)} disabled={bid >= minimalBid * 1000000000000000000 ? false : true} type="button" class="button-main btn btn-primary">Bid</button>
 											</div>
 										</div>
 									</div>
 									<div className="auction-activity row">
 										<div className="col=lg-12">
-											<ul className="history-links">
-												<li className="history-bid">
-													<div className="history-item">
-														<div className='bid-history-left'>
-															<div className="history-bidder">
-																<div>
-																	<div className="history-address">
-																		<div>
-																			<div className='icon'>
-																				<svg width="24px" height="24px">
-																					<rect x="0" y="0" width="24" height="24" fill="#F3E200" transform="translate(-3.161882561253603 4.518394642582742) rotate(265.4 12 12)"></rect>
-																					<rect x="0" y="0" width="24" height="24" fill="#FC9700" transform="translate(11.22026537036266 -4.1929834221118005) rotate(443.7 12 12)"></rect>
-																					<rect x="0" y="0" width="24" height="24" fill="#F76001" transform="translate(-0.637331488665627 16.861902586552223) rotate(201.4 12 12)"></rect>
-																				</svg>
+											{bidders.map((bidder, index) => (
+												<ul className="history-links">
+													<li className="history-bid">
+														<div className="history-item">
+															<div className='bid-history-left'>
+																<div className="history-bidder">
+																	<div>
+																		<div className="history-address">
+																			<div>
+																				<div className='icon'>
+																					<svg width="24px" height="24px">
+																						<rect x="0" y="0" width="24" height="24" fill="#F3E200" transform="translate(-3.161882561253603 4.518394642582742) rotate(265.4 12 12)"></rect>
+																						<rect x="0" y="0" width="24" height="24" fill="#FC9700" transform="translate(11.22026537036266 -4.1929834221118005) rotate(443.7 12 12)"></rect>
+																						<rect x="0" y="0" width="24" height="24" fill="#F76001" transform="translate(-0.637331488665627 16.861902586552223) rotate(201.4 12 12)"></rect>
+																					</svg>
+																				</div>
 																			</div>
+																			{bidder}
 																		</div>
-																		0x2F...6445
 																	</div>
 																</div>
 															</div>
-														</div>
-														<div className="bid-history-right">
-															<div className="history-price">
-																Ξ
-																75.00
-															</div>
-															<div className="history-sale">
-																<a href="/" target="_blank" rel="noreferrer">
-																	<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="external-link-alt" class="svg-inline--fa fa-external-link-alt fa-w-16 " role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-																		<path fill="currentColor" d="M432,320H400a16,16,0,0,0-16,16V448H64V128H208a16,16,0,0,0,16-16V80a16,16,0,0,0-16-16H48A48,48,0,0,0,0,112V464a48,48,0,0,0,48,48H400a48,48,0,0,0,48-48V336A16,16,0,0,0,432,320ZM488,0h-128c-21.37,0-32.05,25.91-17,41l35.73,35.73L135,320.37a24,24,0,0,0,0,34L157.67,377a24,24,0,0,0,34,0L435.28,133.32,471,169c15,15,41,4.5,41-17V24A24,24,0,0,0,488,0Z">
-																		</path>
-																	</svg>
-																</a>
-															</div>
-															<div className="history-link">
-																<a href="https://etherscan.io/tx/0x9fbca796f87b09f7e26224393611afd27d2a11dab6858b50ba40fab2d34751ff" target="_blank" rel="noreferrer"></a>
-															</div>
-														</div>
-													</div>
-												</li>
-											</ul>
-											<ul className="history-links">
-												<li className="history-bid">
-													<div className="history-item">
-														<div className='bid-history-left'>
-															<div className="history-bidder">
-																<div>
-																	<div className="history-address">
-																		<div>
-																			<div className='icon'>
-																				<svg width="24px" height="24px">
-																					<rect x="0" y="0" width="24" height="24" fill="#1583F2" transform="translate(-4.4324274443461364 5.469350116742574) rotate(211.4 12 12)"></rect>
-																					<rect x="0" y="0" width="24" height="24" fill="#FC8D00" transform="translate(-8.925320026703737 -4.391148020231861) rotate(287.7 12 12)"></rect>
-																					<rect x="0" y="0" width="24" height="24" fill="#C7143B" transform="translate(-16.33475440664805 -6.174630902695456) rotate(326.9 12 12)"></rect>
-																				</svg>
-																			</div>
-																		</div>
-																		omakasemoney.eth
-																	</div>
+															<div className="bid-history-right">
+																<div className="history-price">
+																	Ξ
+																	{bids[index] / 1000000000000000000}
+																</div>
+																<div className="history-sale">
+																	<a href="/" target="_blank" rel="noreferrer">
+																		<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="external-link-alt" class="svg-inline--fa fa-external-link-alt fa-w-16 " role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+																			<path fill="currentColor" d="M432,320H400a16,16,0,0,0-16,16V448H64V128H208a16,16,0,0,0,16-16V80a16,16,0,0,0-16-16H48A48,48,0,0,0,0,112V464a48,48,0,0,0,48,48H400a48,48,0,0,0,48-48V336A16,16,0,0,0,432,320ZM488,0h-128c-21.37,0-32.05,25.91-17,41l35.73,35.73L135,320.37a24,24,0,0,0,0,34L157.67,377a24,24,0,0,0,34,0L435.28,133.32,471,169c15,15,41,4.5,41-17V24A24,24,0,0,0,488,0Z">
+																			</path>
+																		</svg>
+																	</a>
+																</div>
+																<div className="history-link">
+																	<a href="https://etherscan.io/tx/0x9fbca796f87b09f7e26224393611afd27d2a11dab6858b50ba40fab2d34751ff" target="_blank" rel="noreferrer"></a>
 																</div>
 															</div>
 														</div>
-														<div className="bid-history-right">
-															<div className="history-price">
-																Ξ
-																47.00
-															</div>
-															<div className="history-sale">
-																<a href="/" target="_blank" rel="noreferrer">
-																	<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="external-link-alt" class="svg-inline--fa fa-external-link-alt fa-w-16 " role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-																		<path fill="currentColor" d="M432,320H400a16,16,0,0,0-16,16V448H64V128H208a16,16,0,0,0,16-16V80a16,16,0,0,0-16-16H48A48,48,0,0,0,0,112V464a48,48,0,0,0,48,48H400a48,48,0,0,0,48-48V336A16,16,0,0,0,432,320ZM488,0h-128c-21.37,0-32.05,25.91-17,41l35.73,35.73L135,320.37a24,24,0,0,0,0,34L157.67,377a24,24,0,0,0,34,0L435.28,133.32,471,169c15,15,41,4.5,41-17V24A24,24,0,0,0,488,0Z">
-																		</path>
-																	</svg>
-																</a>
-															</div>
-															<div className="history-link">
-																<a href="https://etherscan.io/tx/0x9fbca796f87b09f7e26224393611afd27d2a11dab6858b50ba40fab2d34751ff" target="_blank" rel="noreferrer"></a>
-															</div>
-														</div>
-													</div>
-												</li>
-											</ul>
-											<ul className="history-links">
-												<li className="history-bid">
-													<div className="history-item">
-														<div className='bid-history-left'>
-															<div className="history-bidder">
-																<div>
-																	<div className="history-address">
-																		<div>
-																			<div className='icon'>
-																				<svg width="24px" height="24px">
-																					<rect x="0" y="0" width="24" height="24" fill="#01808C" transform="translate(1.732000026293931 -1.1356332663103768) rotate(362.0 12 12)"></rect>
-																					<rect x="0" y="0" width="24" height="24" fill="#034A5D" transform="translate(13.348006048518618 6.3454162277846615) rotate(30.6 12 12)"></rect>
-																					<rect x="0" y="0" width="24" height="24" fill="#F74D01" transform="translate(-12.709997730151008 17.803735889989234) rotate(259.7 12 12)"></rect>
-																				</svg>
-																			</div>
-																		</div>
-																		0xfC...5410
-																	</div>
-																</div>
-															</div>
-														</div>
-														<div className="bid-history-right">
-															<div className="history-price">
-																Ξ
-																8.00
-															</div>
-															<div className="history-sale">
-																<a href="/" target="_blank" rel="noreferrer">
-																	<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="external-link-alt" class="svg-inline--fa fa-external-link-alt fa-w-16 " role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-																		<path fill="currentColor" d="M432,320H400a16,16,0,0,0-16,16V448H64V128H208a16,16,0,0,0,16-16V80a16,16,0,0,0-16-16H48A48,48,0,0,0,0,112V464a48,48,0,0,0,48,48H400a48,48,0,0,0,48-48V336A16,16,0,0,0,432,320ZM488,0h-128c-21.37,0-32.05,25.91-17,41l35.73,35.73L135,320.37a24,24,0,0,0,0,34L157.67,377a24,24,0,0,0,34,0L435.28,133.32,471,169c15,15,41,4.5,41-17V24A24,24,0,0,0,488,0Z">
-																		</path>
-																	</svg>
-																</a>
-															</div>
-															<div className="history-link">
-																<a href="https://etherscan.io/tx/0x9fbca796f87b09f7e26224393611afd27d2a11dab6858b50ba40fab2d34751ff" target="_blank" rel="noreferrer"></a>
-															</div>
-														</div>
-													</div>
-												</li>
-											</ul>
+													</li>
+												</ul>
+											))}
 											<div className="history-btn">
 											</div>
 										</div>
