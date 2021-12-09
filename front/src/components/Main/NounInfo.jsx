570c173c6a79cb36b11d000c1edbee69da763c9e
vit <vit@fanil1.fvds.ru>
2021-12-09 19:31:40 +0300

    fix

--- front/src/components/Main/NounInfo.jsx
+++ front/src/components/Main/NounInfo.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Modal } from 'react-bootstrap';
 import main01 from '../../assets/img/main01.png';
 import MiniCard from '../MiniCard';
